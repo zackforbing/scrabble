16354aa280638fe0ed51bc082b2648ebf26b83c3
Zack Forbing <zackforbing@gmail.com>
2016-05-30 18:00:55 -0600

    finished and added tests

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,6 +1,22 @@
+require 'pry'
 class Scrabble
+
   def score(word)
-    0
+    if word.to_s == ""
+      0
+    elsif word.class != String
+      0
+    else
+      letter_values(wd_split(word)).reduce(:+)
+    end
+  end
+
+  def wd_split(word)
+    word.upcase.split('')
+  end
+
+  def letter_values(word)
+    word.map { |letter| point_values[letter]}
   end
 
   def point_values
@@ -11,7 +27,7 @@ class Scrabble
       "M"=>3, "N"=>1, "O"=>1, "P"=>3,
       "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
       "U"=>1, "V"=>4, "W"=>4, "X"=>8,
-      "Y"=>4, "Z"=>10
+      "Y"=>4, "Z"=>10, ""=>0
     }
   end
 end
--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -5,9 +5,29 @@ require 'minitest/pride'
 require 'pry'
 
 class ScrabbleTest < Minitest::Test
+
   def test_it_can_score_a_single_letter
-    skip
     assert_equal 1, Scrabble.new.score("a")
     assert_equal 4, Scrabble.new.score("f")
+  end
+
+  def test_it_can_score_two_letters
+    assert_equal 4, Scrabble.new.score("ab")
+  end
+
+  def test_ignores_case
+    assert_equal 8, Scrabble.new.score("abAB")
+  end
+
+  def test_gives_zero_for_nil
+    assert_equal 0, Scrabble.new.score(nil)
+  end
+
+  def test_gives_zero_for_empty_string
+    assert_equal 0, Scrabble.new.score("")
+  end
+
+  def test_gives_zero_for_integer
+    assert_equal 0, Scrabble.new.score(1)
   end
 end
